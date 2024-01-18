f3a61f0d81fb0da408d4d531b600208b96669c5e
ounissaSadaoui <120554715+ounissaSadaoui@users.noreply.github.com>
2024-01-18 14:43:26 +0100

    TU dates, suppression des try catch

--- Personnel/src/testsUnitaires/testEmploye.java
+++ Personnel/src/testsUnitaires/testEmploye.java
@@ -68,39 +68,26 @@ class testEmploye {
 		Employe employe = ligue.addEmploye("Bouchar", "Gérar", "bouchard@gmail.com", "zerty", LocalDate.parse("2024-01-13"), LocalDate.parse("2024-01-14"));
 		
 		//test sur le setteur
-		try {
-			employe.setDateArrivee(LocalDate.parse("2020-10-10"));
-			assertEquals(LocalDate.parse("2020-10-10"), employe.getDateArrivee());
-		} catch (DateInvalide e) {}
+		employe.setDateArrivee(LocalDate.parse("2020-10-10"));
+		assertEquals(LocalDate.parse("2020-10-10"), employe.getDateArrivee());
 		
 		//le setteur marche bien
-		
-		try {
-			employe.setDateArrivee(LocalDate.parse("2021-10-10"));
-			assertEquals(LocalDate.parse("2021-10-10"), employe.getDateArrivee());
-		} catch (DateInvalide e) {}
+		employe.setDateArrivee(LocalDate.parse("2021-10-10"));
+		assertEquals(LocalDate.parse("2021-10-10"), employe.getDateArrivee());
 		
 		// on peut mettre une date à null
-		try {
-			employe.setDateArrivee(null);
-			assertEquals(null, employe.getDateArrivee());
+		employe.setDateArrivee(null);
+		assertEquals(null, employe.getDateArrivee());
 
-		} catch (DateInvalide e) {}	
-		
 		// cas ou la date n'est pas renseignée à la création de l'employe
 		 employe = ligue.addEmploye("Bouchar", "Gérar", "bouchard@gmail.com", "zerty", null, null);
 		 assertEquals(null, employe.getDateArrivee());
 		//on peut bien modifier une date qui était à null
 		 
-		 try {
-			employe.setDateArrivee(LocalDate.parse("2022-01-08"));
-			 assertEquals(LocalDate.parse("2022-01-08"), employe.getDateArrivee());
-
-		  } catch (DateInvalide e) {};
-		
+		employe.setDateArrivee(LocalDate.parse("2022-01-08"));
+    	 assertEquals(LocalDate.parse("2022-01-08"), employe.getDateArrivee());
 	}
 	// TESTS sur les Dates d'arrivée et de Depart
-	
 	@Test 
 	// tests sur la date de départ
 	void testSetDateDepart() throws SauvegardeImpossible, DateInvalide
@@ -112,59 +99,43 @@ class testEmploye {
 		assertEquals(null, employe.getDateDepart());
 		
 		//test setteur : cas normal:
-		try {
-			employe.setDateDepart(LocalDate.parse("2024-04-13"));
-		} catch (DateInvalide e) {
-			fail("on ne devrait pas être ici ");
-		}
-
+		employe.setDateDepart(LocalDate.parse("2024-04-13"));
+		
 		// tets setteur : cas date depart antérieure à date arrivee
 		 assertThrows(DateInvalide.class, () -> {
 				employe.setDateDepart(LocalDate.parse("2017-04-13"));
 		 }, "La date d'arrivée ne doit pas être sup à celle de départ");
-
-		
-			//on essaie de mettre une date de depart, alors qu'il n'y a pas de date d'arrivee
+		 
+		//on essaie de mettre une date de depart, alors qu'il n'y a pas de date d'arrivee
 			
-			 assertThrows(DateInvalide.class, () -> {
-					employe.setDateArrivee(null);
-					employe.setDateDepart(LocalDate.parse("2014-08-14"));
-             }, "Il ne peut y avoir de date de depart sans date d'arrivee");
+		 assertThrows(DateInvalide.class, () -> {
+				employe.setDateArrivee(null);
+				employe.setDateDepart(LocalDate.parse("2014-08-14"));
+         }, "Il ne peut y avoir de date de depart sans date d'arrivee");
 			 
 			//la dernière date inscrite est bien celle qui est conservée
-			assertEquals(LocalDate.parse("2024-04-13"), employe.getDateDepart());
-		try {
+		assertEquals(LocalDate.parse("2024-04-13"), employe.getDateDepart());
 			//on remet la date à null
-			employe.setDateDepart(null);
-			assertEquals(null, employe.getDateDepart());
-		}catch (DateInvalide e) {		}
+		employe.setDateDepart(null);
+		assertEquals(null, employe.getDateDepart());
 		
 		
 		//on essaie une date de depart null et une quelconque date d'arrivee
-		try {
-			employe.setDateArrivee(LocalDate.parse("2012-12-12"));
-			employe.setDateDepart(null);
+		employe.setDateArrivee(LocalDate.parse("2012-12-12"));
+		employe.setDateDepart(null);
 
-		}catch (DateInvalide e) {}
-		
-		try {
-			employe.setDateArrivee(LocalDate.parse("2012-12-12"));
-			employe.setDateDepart(LocalDate.parse("2012-12-13"));
-			assertEquals(LocalDate.parse("2012-12-13"), employe.getDateDepart());
+		employe.setDateArrivee(LocalDate.parse("2012-12-12"));
+		employe.setDateDepart(LocalDate.parse("2012-12-13"));
+		assertEquals(LocalDate.parse("2012-12-13"), employe.getDateDepart());
 
-		}catch (DateInvalide e) {}
+		employe.setDateDepart(LocalDate.parse("2024-02-13"));
+			
+		assertEquals(LocalDate.parse("2024-02-13"), employe.getDateDepart());
 		
-		try {
-			employe.setDateDepart(LocalDate.parse("2024-02-13"));
+		employe.setDateDepart(LocalDate.parse("2024-04-13"));
 			
-			assertEquals(LocalDate.parse("2024-02-13"), employe.getDateDepart());
-			
-			employe.setDateDepart(LocalDate.parse("2024-04-13"));
-			
-			assertEquals(LocalDate.parse("2024-04-13"), employe.getDateDepart());
-		} catch (DateInvalide e) {} 
-		
-}
+		assertEquals(LocalDate.parse("2024-04-13"), employe.getDateDepart());
+		}	
 }
 
 
