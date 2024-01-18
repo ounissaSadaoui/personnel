0eab3b365138ac88551bd334fa8d4d49e47374f3
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-01-18 22:56:47 +0100

    TU class employe

--- Personnel/src/testsUnitaires/testEmploye.java
+++ Personnel/src/testsUnitaires/testEmploye.java
@@ -18,6 +18,61 @@ class testEmploye {
 	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
 	 
 	
+	//tests sur les formats de dates possibles
+		@Test
+		void testCreationEmploye() throws SauvegardeImpossible, DateInvalide
+		{
+			Ligue ligue1 = gestionPersonnel.addLigue("Ligue1");
+			
+			Employe employe1 = ligue1.addEmploye("nom", "prénom", "adresse@mail.com", "mdp", LocalDate.parse("2024-01-13"), LocalDate.parse("2025-01-13"));
+			assertEquals(ligue1, employe1.getLigue());
+			assertEquals("nom", employe1.getNom());
+			assertEquals("prénom", employe1.getPrenom());
+			assertEquals("adresse@mail.com", employe1.getMail());
+			assertTrue(employe1.checkPassword("mdp"));
+			assertEquals(LocalDate.parse("2024-01-13"), employe1.getDateArrivee());
+			assertEquals(LocalDate.parse("2025-01-13"), employe1.getDateDepart());
+			
+			Employe employe2 = ligue1.addEmploye("nom2", "prénom2", "adresse2@mail.com", "mdp2", LocalDate.parse("2022-01-13"), LocalDate.parse("2023-01-13"));
+			assertEquals(ligue1, employe2.getLigue());
+			assertEquals("nom2", employe2.getNom());
+			assertEquals("prénom2", employe2.getPrenom());
+			assertEquals("adresse2@mail.com", employe2.getMail());
+			assertTrue(employe2.checkPassword("mdp2"));
+			assertEquals(LocalDate.parse("2022-01-13"), employe2.getDateArrivee());
+			assertEquals(LocalDate.parse("2023-01-13"), employe2.getDateDepart());
+			
+			Ligue ligueA = gestionPersonnel.addLigue("Liga");
+			
+			Employe employeA = ligueA.addEmploye("lastname", "firstname", "mail@mail.com", "pwd", LocalDate.parse("2010-01-13"), LocalDate.parse("2011-01-13"));
+			assertEquals(ligueA, employeA.getLigue());
+			assertEquals("lastname", employeA.getNom());
+			assertEquals("firstname", employeA.getPrenom());
+			assertEquals("mail@mail.com", employeA.getMail());
+			assertTrue(employeA.checkPassword("pwd"));
+			assertEquals(LocalDate.parse("2010-01-13"), employeA.getDateArrivee());
+			assertEquals(LocalDate.parse("2011-01-13"), employeA.getDateDepart());
+			
+			Employe employeB = ligueA.addEmploye("lastname2", "firstname2", "mail2@mail.com", "pwd2", LocalDate.parse("2012-01-13"), LocalDate.parse("2013-01-13"));
+			assertEquals(ligueA, employeB.getLigue());
+			assertEquals("lastname2", employeB.getNom());
+			assertEquals("firstname2", employeB.getPrenom());
+			assertEquals("mail2@mail.com", employeB.getMail());
+			assertTrue(employeB.checkPassword("pwd2"));
+			assertEquals(LocalDate.parse("2012-01-13"), employeB.getDateArrivee());
+			assertEquals(LocalDate.parse("2013-01-13"), employeB.getDateDepart());
+			
+			
+			// test format de date arrivée pas dans le bon format 
+			assertThrows(DateTimeParseException.class, () -> {
+				Employe goat = ligue1.addEmploye("Jude", "Belli", "heyJude@rm.com", "goal", null, LocalDate.parse("dat-invalide"));
+		    }, "la date de départ doit être au format yyyy-mm-dd.");	
+			
+			assertThrows(DateInvalide.class, () -> {
+				Employe goat = ligue1.addEmploye("Jude", "Belli", "heyJude@rm.com", "goal", LocalDate.parse("2024-01-13"), LocalDate.parse("2020-01-13"));
+		    }, "la date de départ doit être postérieur a la date d'arrivée.");	
+		}
+	
 	// test avec assertThrows sur le setteur dateArrivee
 	@Test
 	  void testsurSetDateArrivee() throws SauvegardeImpossible, DateInvalide{
@@ -40,6 +95,7 @@ class testEmploye {
 	    assertEquals(LocalDate.parse("2020-10-10"), goat.getdateArrivee());
 	    }, "Le test devrait fonctionner.");*/
 	   }
+	
     @Test
     void testSetdateArrivee_InvalidDate() throws SauvegardeImpossible {
         Ligue liga = gestionPersonnel.addLigue("Liga");
@@ -49,17 +105,7 @@ class testEmploye {
             Employe goat = liga.addEmploye("Jude", "Belli", "heyJude@rm.com", "goal", LocalDate.parse("2021-05-0"), null);
             }, "La format de date d'arrivée doit être YYYY-MM-DD");
     }
-	//tests sur les formats de dates possibles
-	@Test
-	void testCreationEmploye() throws SauvegardeImpossible, DateInvalide
-	{
-        // test format de date arrivée pas dans le bon format 
-		 Ligue liga = gestionPersonnel.addLigue("Liga");	
- 		 assertThrows(DateTimeParseException.class, () -> {
- 	 	     Employe goat = liga.addEmploye("Jude", "Belli", "heyJude@rm.com", "goal", null, LocalDate.parse("dat-invalide"));
-	        }, "la date de départ doit ẑtre au format yyyy-mm-dd.");	
-	}
-	
+    
 	@Test 
 	//TU sur la dateArrivee
 	void testSetDateArrivee() throws SauvegardeImpossible, DateInvalide
@@ -87,7 +133,9 @@ class testEmploye {
 		employe.setDateArrivee(LocalDate.parse("2022-01-08"));
     	 assertEquals(LocalDate.parse("2022-01-08"), employe.getDateArrivee());
 	}
+	
 	// TESTS sur les Dates d'arrivée et de Depart
+	
 	@Test 
 	// tests sur la date de départ
 	void testSetDateDepart() throws SauvegardeImpossible, DateInvalide
@@ -135,7 +183,70 @@ class testEmploye {
 		employe.setDateDepart(LocalDate.parse("2024-04-13"));
 			
 		assertEquals(LocalDate.parse("2024-04-13"), employe.getDateDepart());
-		}	
+	}	
+	
+	@Test
+	void testSetNom() throws SauvegardeImpossible, DateInvalide {
+		Ligue ligue = gestionPersonnel.addLigue("Ligue1");
+		Employe employe = ligue.addEmploye("Vinicius", "Junior", "vini.jr@gmail.com", "azerty", null, null);
+		
+		employe.setNom("matuidi");
+		assertEquals("matuidi", employe.getNom());
+		
+		employe.setNom("verratti");
+		assertEquals("verratti", employe.getNom());
+		
+		employe.setNom(null);
+		assertNull(employe.getNom());
+	}
+	
+	@Test
+	void testSetPrenom() throws SauvegardeImpossible, DateInvalide {
+		Ligue ligue = gestionPersonnel.addLigue("Ligue1");
+		Employe employe = ligue.addEmploye("Vinicius", "Junior", "vini.jr@gmail.com", "azerty", null, null);
+		
+		employe.setPrenom("blaise");
+		assertEquals("blaise", employe.getPrenom());
+		
+		employe.setPrenom("marco");
+		assertEquals("marco", employe.getPrenom());
+		
+		employe.setPrenom(null);
+		assertNull(employe.getPrenom());
+	}
+	
+	@Test
+	void testSetMail() throws SauvegardeImpossible, DateInvalide {
+		Ligue ligue = gestionPersonnel.addLigue("Liga");
+		Employe employe = ligue.addEmploye("Vinicius", "Junior", "vini.jr@gmail.com", "azerty", null, null);
+		
+		employe.setMail("matuidi@mail.com");
+		assertEquals("matuidi@mail.com", employe.getMail());
+		
+		employe.setMail("marco@mail.com");
+		assertEquals("marco@mail.com", employe.getMail());
+		
+		employe.setMail(null);
+		assertNull(employe.getMail());
+	}
+	
+	@Test
+	void testCheckPassword() throws SauvegardeImpossible, DateInvalide {
+		Ligue ligue = gestionPersonnel.addLigue("Liga");
+		Employe employe = ligue.addEmploye("Vinicius", "Junior", "vini.jr@gmail.com", "azerty", null, null);
+		
+		assertTrue(employe.checkPassword("azerty"));
+		assertFalse(employe.checkPassword("azertyuiop"));
+		
+		employe.setPassword("azertyuiop");
+		assertTrue(employe.checkPassword("azertyuiop"));
+		assertFalse(employe.checkPassword("azerty"));
+		
+		employe.setPassword("supermdp");
+		assertTrue(employe.checkPassword("supermdp"));
+		assertFalse(employe.checkPassword("azerty"));
+		assertFalse(employe.checkPassword("azertyuiop"));
+	}
 }
 
 
