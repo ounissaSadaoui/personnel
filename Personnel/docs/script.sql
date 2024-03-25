b345120140a79f0283311d98785f6f1c0d111502
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-03-25 21:15:19 +0100

    script sql

--- Personnel/docs/script.sql
+++ Personnel/docs/script.sql
@@ -1,22 +1,60 @@
-CREATE TABLE employe (
-    idEmploye INT PRIMARY KEY,
-    nomEmploye VARCHAR(255),
-    prenomEmploye VARCHAR(255),
-    mailEmploye VARCHAR(255),
-    passwordEmploye VARCHAR(255),
-    dateArrivee DATE,
-    dateDepart DATE,
-    idLigue INT
+CREATE TABLE `employe` (
+  `idEmploye` int NOT NULL,
+  `nom` varchar(255),
+  `prenom` varchar(255),
+  `mail` varchar(255),
+  `password` varchar(255),
+  `dateArrivee` date DEFAULT NULL,
+  `dateDepart` date DEFAULT NULL,
+  `idLigue` int DEFAULT NULL
 );
 
-CREATE TABLE ligue (
-    idLigue INT PRIMARY KEY,
-    nomLigue VARCHAR(255),
-    idEmploye INT
+
+CREATE TABLE `ligue` (
+  `idLigue` int NOT NULL,
+  `nom` varchar(255),
+  `idEmploye` int DEFAULT NULL
 );
-ALTER TABLE employe
-ADD FOREIGN KEY (idLigue) REFERENCES ligue(idLigue);
 
-ALTER TABLE ligue
-ADD FOREIGN KEY (idEmploye) REFERENCES employe(idEmploye);
+ALTER TABLE `employe`
+  ADD PRIMARY KEY (`idEmploye`),
+  ADD KEY `employe_ibfk_1` (`idLigue`);
 
+--
+-- Index pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  ADD PRIMARY KEY (`idLigue`),
+  ADD KEY `ligue_ibfk_1` (`idEmploye`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `employe`
+--
+ALTER TABLE `employe`
+  MODIFY `idEmploye` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=144;
+
+--
+-- AUTO_INCREMENT pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  MODIFY `idLigue` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `employe`
+--
+ALTER TABLE `employe`
+  ADD CONSTRAINT `employe_ibfk_1` FOREIGN KEY (`idLigue`) REFERENCES `ligue` (`idLigue`);
+
+--
+-- Contraintes pour la table `ligue`
+--
+ALTER TABLE `ligue`
+  ADD CONSTRAINT `ligue_ibfk_1` FOREIGN KEY (`idEmploye`) REFERENCES `employe` (`idEmploye`);
