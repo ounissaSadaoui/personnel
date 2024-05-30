460174a4d1586f0ca7c865cd2f75ed3034f99601
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-05-31 01:40:06 +0200

    color

--- Personnel/src/gui/Frame.java
+++ Personnel/src/gui/Frame.java
@@ -1,6 +1,11 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
 	
@@ -10,6 +15,12 @@ public class Frame extends JFrame {
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(600, 400);
 		this.setLocationRelativeTo(null);
+		
+		JPanel connexion = (JPanel) this.getContentPane();
+		connexion.setLayout(new FlowLayout());
+		connexion.add(new JButton("Connexion"));
+		
+		connexion.setBackground(new Color(12, 23, 40));
 		
 	}
 	
