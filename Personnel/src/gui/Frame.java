429624ac024b2ca7891dbbe70f3c26f341183160
Cynthia Apura <120573517+cynthiaapura@users.noreply.github.com>
2024-05-31 02:43:22 +0200

    frame field

--- Personnel/src/gui/Frame.java
+++ Personnel/src/gui/Frame.java
@@ -1,27 +1,46 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Frame extends JFrame {
 	
 	public Frame() {
 		
-		super("Connexion LDO");
+		super("LDO - Ligues Dynamiques et organisées");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(600, 400);
 		this.setLocationRelativeTo(null);
-		
-		JPanel connexion = (JPanel) this.getContentPane();
-		connexion.setLayout(new FlowLayout());
-		connexion.add(new JButton("Connexion"));
-		
-		connexion.setBackground(new Color(12, 23, 40));
-		
+	
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        JTextField userField = new JTextField("Nom", 20);
+        contentPane.add(userField);
+        userField.setLayout(new FlowLayout(FlowLayout.CENTER));
+        userField.setPreferredSize(new Dimension(100, 30)); 
+        
+        // creation et position du bouton
+        JButton button = new JButton("Connexion");
+        button.setPreferredSize(new Dimension(100, 30)); 
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(button);
+
+        contentPane.add(buttonPanel, BorderLayout.SOUTH);
+
+        contentPane.setBackground(new Color(12, 23, 40));
+        buttonPanel.setBackground(new Color(12, 23, 40));
+   
 	}
 	
 	public static void main(String[] args) {
